4bd54ccf8f380e8091b2b545b0fa06c512a50ec8
dynamiccookies <dynamiccookies@users.noreply.github.com>
2020-04-14 21:52:43 -0500

    Added File Upload & Alphabetized CSS (#2)
    - Added file upload (#2) to call `parseFile` function
    - Alphabetized CSS
    
    Close #2

--- index.php
+++ index.php
@@ -1,69 +1,69 @@
 <?php
 
-    /********** Enter Your Values Here **********/
+    $msgs = array();
+    $rcvNum = '';
 
-    $SMSXMLFilename = '';   /* Your Filename */
+    if (isset($_POST['upload'])) {$msgs = parseFile($_FILES['xmlfile']['tmp_name']);}
 
-    /******* Don't Modify Below This Line *******/
+    function parseFile($filename) {
+        $file    = file_get_contents($filename);
+        $results = preg_replace_callback('/(&#\d{5};){2}/', function($matches){return convertToEmoji($matches);}, $file);
+        $tmpfile = $_SERVER['TMPDIR'] . '/' . substr(md5(uniqid()),0,6) . '.xml';
+        $reader  = new XMLReader();
+        $doc     = new DOMDocument;
 
-    $file    = file_get_contents($SMSXMLFilename);
-    $results = preg_replace_callback('/(&#\d{5};){2}/', function($matches){return convertToEmoji($matches);}, $file);
-    $tmpfile = $_SERVER['TMPDIR'] . '/' . substr(md5(uniqid()),0,6) . '.xml';
-    $reader  = new XMLReader();
-    $doc     = new DOMDocument;
-    $msgs    = array();
-	$rcvNum	 = '';
-
-    file_put_contents($tmpfile,$results);
-
-    if (!$reader->open($tmpfile)) {die('Failed to open XML file.');}
-
-    while($reader->read()) {
-        if ($reader->nodeType == XMLReader::ELEMENT && ($reader->name == 'sms')) {
-
-            if ($reader->getAttribute('type') == 1)     {$type = 'received';}
-            elseif ($reader->getAttribute('type') == 2) {$type = 'sent';}
-            else {$type = 'notype';}
-
-			$msgs[] = array(
-				'type'          => $type,
-				'text'          => $reader->getAttribute('body'),
-				'date'          => $reader->getAttribute('date'),
-				'address'       => formatPhoneNumber($reader->getAttribute('address')),
-				'contact_name'  => $reader->getAttribute('contact_name'),
-				'readable_date' => $reader->getAttribute('readable_date')
-			);
-
-        } elseif ($reader->nodeType == XMLReader::ELEMENT && ($reader->name == 'mms')) {
-
-            if ($reader->getAttribute('msg_box') == 1)     {$type = 'received';}
-            elseif ($reader->getAttribute('msg_box') == 2) {$type = 'sent';}
-            else {$type = 'notype';}
-
-			$body = '';
-            $node = simplexml_import_dom($doc->importNode($reader->expand(), true));
-            foreach ($node->parts->part as $part) {
-                if  ($part['ct'] == 'image/png') 	{$body .= "<img src='data:image/png;base64, " . $part['data'] . "' alt='" . $part['name'] . "'><br>";}
-				elseif ($part['ct'] == 'image/jpeg'){$body .= "<img src='data:image/jpeg;base64, " . $part['data'] . "' alt='" . $part['name'] . "'><br>";}
-				elseif ($part['ct'] == 'text/plain'){$body .= $part['text'] . '<br>';}
+        file_put_contents($tmpfile,$results);
+    
+        if (!$reader->open($tmpfile)) {die('Failed to open XML file.');}
+    
+        while($reader->read()) {
+            if ($reader->nodeType == XMLReader::ELEMENT && ($reader->name == 'sms')) {
+    
+                if ($reader->getAttribute('type') == 1)     {$type = 'received';}
+                elseif ($reader->getAttribute('type') == 2) {$type = 'sent';}
+                else {$type = 'notype';}
+    
+    			$msgs[] = array(
+    				'type'          => $type,
+    				'text'          => $reader->getAttribute('body'),
+    				'date'          => $reader->getAttribute('date'),
+    				'address'       => formatPhoneNumber($reader->getAttribute('address')),
+    				'contact_name'  => $reader->getAttribute('contact_name'),
+    				'readable_date' => $reader->getAttribute('readable_date')
+    			);
+    
+            } elseif ($reader->nodeType == XMLReader::ELEMENT && ($reader->name == 'mms')) {
+    
+                if ($reader->getAttribute('msg_box') == 1)     {$type = 'received';}
+                elseif ($reader->getAttribute('msg_box') == 2) {$type = 'sent';}
+                else {$type = 'notype';}
+    
+    			$body = '';
+                $node = simplexml_import_dom($doc->importNode($reader->expand(), true));
+                foreach ($node->parts->part as $part) {
+                    if  ($part['ct'] == 'image/png') 	{$body .= "<img src='data:image/png;base64, " . $part['data'] . "' alt='" . $part['name'] . "'><br>";}
+    				elseif ($part['ct'] == 'image/jpeg'){$body .= "<img src='data:image/jpeg;base64, " . $part['data'] . "' alt='" . $part['name'] . "'><br>";}
+    				elseif ($part['ct'] == 'text/plain'){$body .= $part['text'] . '<br>';}
+                }
+    
+    			if (!$rcvNum) {foreach ($node->addrs->addr as $addr) {if ($addr['type'] == '151') {$rcvNum = formatPhoneNumber($addr['address']);}}}
+    
+    			$msgs[] = array(
+    				'type'          => $type,
+    				'text'          => $body,
+    				'date'          => $reader->getAttribute('date'),
+    				'address'       => formatPhoneNumber($reader->getAttribute('address')),
+    				'contact_name'  => $reader->getAttribute('contact_name'),
+    				'readable_date' => $reader->getAttribute('readable_date')
+    			);
             }
-
-			if (!$rcvNum) {foreach ($node->addrs->addr as $addr) {if ($addr['type'] == '151') {$rcvNum = formatPhoneNumber($addr['address']);}}}
-
-			$msgs[] = array(
-				'type'          => $type,
-				'text'          => $body,
-				'date'          => $reader->getAttribute('date'),
-				'address'       => formatPhoneNumber($reader->getAttribute('address')),
-				'contact_name'  => $reader->getAttribute('contact_name'),
-				'readable_date' => $reader->getAttribute('readable_date')
-			);
         }
+    
+		$reader->close();
+		unlink($tmpfile);
+		array_multisort(array_column($msgs, 'date'), $msgs);
+		return $msgs;
     }
-
-    $reader->close();
-	unlink($tmpfile);
-    array_multisort(array_column($msgs, 'date'), $msgs);
 
     function convertToEmoji($matches){
         $newStr     = $matches[0];
@@ -87,15 +87,15 @@
         <title>SMS XML Parser</title>
         <style>
             body {
-            	background-color:black;
-            	color:white;
-            	margin:auto;
-            	max-width:90%;
+            	background-color: black;
+            	color: white;
+            	margin: auto;
+            	max-width: 90%;
             }
             img {
-                border:1px solid darkslategray;
-            	margin-top:5px;
-            	max-width:400px;
+                border: 1px solid darkslategray;
+            	margin-top: 5px;
+            	max-width: 400px;
             }
             img:hover {
             	-moz-transform: scale(1.5);
@@ -113,52 +113,69 @@
             	transform: scale(1.5);
                 transform-origin: top left;
             }
-            .details {font-weight:bold;}
+            .details {font-weight: bold;}
             .received {
             	-khtml-border-radius: 0 20px 20px;
             	-moz-border-radius: 0 20px 20px;
             	-webkit-border-radius: 0 20px 20px;
-            	background-color:lightgrey;
+            	background-color: lightgrey;
             	border-radius: 0 20px 20px;
-            	float:left;
+            	float: left;
             }
             .sent, .received {
-            	clear:both;
-            	color:black;
-            	margin:10px;
-            	max-width:75%;
-            	padding:15px;
-            	text-align:justify;
+            	clear: both;
+            	color: black;
+            	margin: 10px;
+            	max-width: 75%;
+            	padding: 15px;
+            	text-align: justify;
             }
             .sent {
             	-khtml-border-radius: 20px 0 20px 20px;
             	-moz-border-radius: 20px 0 20px 20px;
             	-webkit-border-radius: 20px 0 20px 20px;
-            	background-color:lightblue;
+            	background-color: lightblue;
             	border-radius: 20px 0 20px 20px;
-            	float:right;
+            	float: right;
             }
-            .sent .details {float:right;}
+            .sent .details {float: right;}
             .sent img:hover {transform-origin: top right;}
+            #uploadForm {
+                background-color: #eee;
+                border-radius: 10px;
+                color: black;
+                margin: 20px;
+                padding: 20px;
+                text-align: center;
+            }
 
             @media print {
             	body {
-            		background-color:white;
-            		color:black;
+            		background-color: white;
+            		color: black;
             	}
-            	div {page-break-inside:avoid;}
+            	div {page-break-inside: avoid;}
+            	#uploadForm {display: none;}
             }
         </style>
     </head>
     <body>
-        <h1>Text Message History</h1>
-        <?php
-			foreach ($msgs as $msg) {
-				echo "\t\t<div class='" . $msg['type'] . "'><span class='details'>" . $msg['readable_date'];
-				if ($msg['type'] == 'received') {echo ' - ' . $msg['contact_name'] . ' - ' . $msg['address'];}
-				elseif ($msg['type'] == 'sent') {echo ' - Me' . ($rcvNum ? ' - ' . $rcvNum : '');}
-                print_r('</span><br>' . $msg['text'] . "</div>\n");
-			}
-		?>
+        <div id='uploadForm'>
+            <form name='fileUpload' method='post' action='' enctype='multipart/form-data'>
+        		<input type='file' name='xmlfile' accept='.xml,xml/*,text/xml'>
+        		<input type='submit' name='upload' value='Upload'>
+    		</form>
+        </div>
+        <?php if ($msgs){?>
+            <h1>Text Message History</h1>
+            <?php
+    			foreach ($msgs as $msg) {
+    				echo "\t\t<div class='" . $msg['type'] . "'><span class='details'>" . $msg['readable_date'];
+    				if ($msg['type'] == 'received') {echo ' - ' . $msg['contact_name'] . ' - ' . $msg['address'];}
+    				elseif ($msg['type'] == 'sent') {echo ' - Me' . ($rcvNum ? ' - ' . $rcvNum : '');}
+                    print_r('</span><br>' . $msg['text'] . "</div>\n");
+    			}
+    		?>
+        <?php }?>
     </body>
 </html>
