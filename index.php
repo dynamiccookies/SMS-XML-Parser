ce410fc430f47d96213b8a313b735ec3c0e302b5
dynamiccookies <dynamiccookies@users.noreply.github.com>
2020-04-17 23:40:28 -0500

    Added CSS loading image (#3)
    - Instead of using a GIF that would be linked (internally or externally) or base64 encoding a loading image, decided to tweak and use really simple CSS loading spinner from https://loading.io/css that is CC0 licensed
    - This also partially solves Issue #12 for when the XML file is being parsed - just need to figure out how to show loading/progress for actually file upload
    
    Close #3

--- index.php
+++ index.php
@@ -171,6 +171,32 @@
 				text-align: center;
 				width: 50%;
             }
+            .lds-ring {
+                margin: auto;
+                display: block;
+                position: relative;
+                width: 80px;
+                height: 80px;
+            }
+            .lds-ring div {
+                box-sizing: border-box;
+                display: block;
+                position: absolute;
+                width: 64px;
+                height: 64px;
+                margin: 8px;
+                border: 8px solid #fff;
+                border-radius: 50%;
+                animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+                border-color: #fff transparent transparent transparent;
+            }
+            .lds-ring div:nth-child(1) {animation-delay: -0.45s;}
+            .lds-ring div:nth-child(2) {animation-delay: -0.3s;}
+            .lds-ring div:nth-child(3) {animation-delay: -0.15s;}
+            @keyframes lds-ring {
+                0%   {transform: rotate(0deg);}
+                100% {transform: rotate(360deg);}
+            }
 
             @media print {
             	body {
@@ -179,10 +205,12 @@
             	}
             	div {page-break-inside: avoid;}
             	#uploadForm {display: none;}
+            	#loading {display: none;}
             }
         </style>
     </head>
     <body>
+        <div id='loading' class='lds-ring'><div></div><div></div><div></div><div></div></div>
         <div id='uploadForm'>
             <form name='fileUpload' method='post' action='' enctype='multipart/form-data'>
         		<input type='file' name='xmlfile' accept='.xml,xml/*,text/xml'>
